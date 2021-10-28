e8e1e585b8b523cc618b64137cbcdc87ff2fe89c
NeskladAnya <belko.hanna.off@gmail.com>
2021-10-28 13:39:17 +0200

    Create the Test model

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_28_113553) do
+ActiveRecord::Schema.define(version: 2021_10_28_113721) do
 
   create_table "categories", force: :cascade do |t|
     t.string "title", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "tests", force: :cascade do |t|
+    t.string "title", null: false
+    t.integer "level", default: 1, null: false
+    t.integer "category_id", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["category_id"], name: "index_tests_on_category_id"
+    t.index ["user_id"], name: "index_tests_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -24,4 +35,6 @@ ActiveRecord::Schema.define(version: 2021_10_28_113553) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "tests", "categories"
+  add_foreign_key "tests", "users"
 end
