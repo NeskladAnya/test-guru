20e81e2fcc78eaea0c0366ee47e360bb8e92673b
NeskladAnya <belko.hanna.off@gmail.com>
2021-10-29 21:45:48 +0200

    Create the self.find_test method

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,7 @@
 class Test < ApplicationRecord
+
+  def self.find_test(category)
+    name = Category.find_by(title: category)
+    Test.select(:title).where(category_id: name.id).order(title: :desc)
+  end
 end
