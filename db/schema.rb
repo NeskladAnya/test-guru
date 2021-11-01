aeec1eb7c82d68fe9d73aaa40c5cadfba80fa9ab
NeskladAnya <belko.hanna.off@gmail.com>
2021-11-01 07:53:14 +0100

    Create new seeded data

--- db/schema.rb
+++ db/schema.rb
@@ -66,6 +66,4 @@ ActiveRecord::Schema.define(version: 2021_10_30_191853) do
   add_foreign_key "questions", "tests"
   add_foreign_key "tests", "categories"
   add_foreign_key "tests", "users", column: "author_id"
-  add_foreign_key "results", "users"
-  add_foreign_key "results", "tests"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,26 +1,58 @@
 categories = 5.times do |i|
-  Category.create(title: "Category #{i}")
+  Category.create!(title: "Category #{i}")
 end
 
 users = 3.times do |i|
-  User.create(name: "User #{i}")
+  User.create!(name: "User #{i}")
 end
 
-tests = 10.times do |i|
-  Test.create(title: "Test name #{i}", level: rand(6), category_id: rand(1..5), author_id: rand(1..5))
-end
+tests = Test.create!(
+  [
+    { title: "Ruby on Rails", level: 1, category_id: 2, author_id: 3 },
+    { title: "School math", level: 2, category_id: 1, author_id: 1 },
+    { title: "Music", level: 0, category_id: 3, author_id: 1 },
+    { title: "Art", level: 5, category_id: 3, author_id: 2 },
+    { title: "HTML", level: 3, category_id: 2, author_id: 2 }
+  ]
+)
 
-questions = 15.times do |i|
-  Question.create(body: "Question #{i}", test_id: rand(1..10))
-end
+questions = Question.create!(
+  [
+    { body: "What is your favourite subject?", test_id: 1 },
+    { body: "2 + 2 = ?", test_id: 2 },
+    { body: "Where was Claude Monet born?", test_id: 4 },
+    { body: "What is <p>?", test_id: 5 },
+    { body: "What is [1, 2, 3]?", test_id: 1 },
+    { body: "What is Freddie Mercury's real name?", test_id: 3 },
+    { body: "5**3 = ?", test_id: 2 },
+    { body: "5 + 2 * 3 = ?", test_id: 2 },
+    { body: "x equals to? x + 10 * 2 = 25", test_id: 2 }
+  ]
+)
 
-answers = 10.times do |i|
-  Answer.create(body: "Answer #{i}", correct: rand(2) == 1 ? true : false, question_id: rand(1..15))
-end
-
-results = 10.times do |i|
-  Result.create(user_id: rand(1..3), test_id: rand(1..10))
-end
+answers = Answer.create!(
+  [
+    { body: "Math", correct: true, question_id: 1 },
+    { body: "Music", correct: false, question_id: 1 },
+    { body: "4", correct: true, question_id: 2 },
+    { body: "5", correct: false, question_id: 2 },
+    { body: "France", correct: true, question_id: 3 },
+    { body: "Germany", correct: false, question_id: 3 },
+    { body: "France", correct: true, question_id: 3 },
+    { body: "Paragraph tag", correct: true, question_id: 4 },
+    { body: "Party", correct: false, question_id: 4 },
+    { body: "Hash", correct: false, question_id: 5 },
+    { body: "Array", correct: true, question_id: 5 },
+    { body: "Farrokh Bulsara", correct: true, question_id: 6 },
+    { body: "Freddie Mercury", correct: false, question_id: 6 },
+    { body: "125", correct: true, question_id: 7 },
+    { body: "225", correct: false, question_id: 7 },
+    { body: "11", correct: true, question_id: 8 },
+    { body: "21", correct: false, question_id: 8 },
+    { body: "15", correct: false, question_id: 9 },
+    { body: "5", correct: true, question_id: 9 },
+  ]
+)
 
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
