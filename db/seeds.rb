46eb679259e7eeb1eb615054ecc08dca0d1b61f1
NeskladAnya <belko.hanna.off@gmail.com>
2021-11-01 19:48:10 +0100

    Modify the seeded data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,56 +1,63 @@
-categories = 5.times do |i|
-  Category.create!(title: "Category #{i}")
-end
+categories = Category.create!(
+  [
+    { title: "Programming" },
+    { title: "School" },
+    { title: "Art" }
+  ]
+)
 
-users = 3.times do |i|
-  User.create!(name: "User #{i}")
-end
+users = User.create!(
+  [
+    { name: "Peter" },
+    { name: "Karl" },
+    { name: "Mia" }
+  ]
+)
 
 tests = Test.create!(
   [
-    { title: "Ruby on Rails", level: 1, category_id: 2, author_id: 3 },
-    { title: "School math", level: 2, category_id: 1, author_id: 1 },
-    { title: "Music", level: 0, category_id: 3, author_id: 1 },
-    { title: "Art", level: 5, category_id: 3, author_id: 2 },
-    { title: "HTML", level: 3, category_id: 2, author_id: 2 }
+    { title: "Ruby on Rails", level: 1, category_id: categories[1].id, author_id: users[2].id },
+    { title: "School math", level: 2, category_id: categories[0].id, author_id: users[0].id },
+    { title: "Music", level: 0, category_id: categories[2].id, author_id: users[0].id },
+    { title: "Art", level: 5, category_id: categories[2].id, author_id: users[1].id },
+    { title: "HTML", level: 3, category_id: categories[1].id, author_id: users[1].id }
   ]
 )
 
 questions = Question.create!(
   [
-    { body: "What is your favourite subject?", test_id: 1 },
-    { body: "2 + 2 = ?", test_id: 2 },
-    { body: "Where was Claude Monet born?", test_id: 4 },
-    { body: "What is <p>?", test_id: 5 },
-    { body: "What is [1, 2, 3]?", test_id: 1 },
-    { body: "What is Freddie Mercury's real name?", test_id: 3 },
-    { body: "5**3 = ?", test_id: 2 },
-    { body: "5 + 2 * 3 = ?", test_id: 2 },
-    { body: "x equals to? x + 10 * 2 = 25", test_id: 2 }
+    { body: "What is your favourite subject?", test_id: tests[0].id },
+    { body: "2 + 2 = ?", test_id: tests[1].id },
+    { body: "Where was Claude Monet born?", test_id: tests[3].id },
+    { body: "What is <p>?", test_id: tests[4].id },
+    { body: "What is [1, 2, 3]?", test_id: tests[0].id },
+    { body: "What is Freddie Mercury's real name?", test_id: tests[2].id },
+    { body: "5**3 = ?", test_id: tests[1].id },
+    { body: "5 + 2 * 3 = ?", test_id: tests[1].id },
+    { body: "x equals to? x + 10 * 2 = 25", test_id: tests[1].id }
   ]
 )
 
 answers = Answer.create!(
   [
-    { body: "Math", correct: true, question_id: 1 },
-    { body: "Music", correct: false, question_id: 1 },
-    { body: "4", correct: true, question_id: 2 },
-    { body: "5", correct: false, question_id: 2 },
-    { body: "France", correct: true, question_id: 3 },
-    { body: "Germany", correct: false, question_id: 3 },
-    { body: "France", correct: true, question_id: 3 },
-    { body: "Paragraph tag", correct: true, question_id: 4 },
-    { body: "Party", correct: false, question_id: 4 },
-    { body: "Hash", correct: false, question_id: 5 },
-    { body: "Array", correct: true, question_id: 5 },
-    { body: "Farrokh Bulsara", correct: true, question_id: 6 },
-    { body: "Freddie Mercury", correct: false, question_id: 6 },
-    { body: "125", correct: true, question_id: 7 },
-    { body: "225", correct: false, question_id: 7 },
-    { body: "11", correct: true, question_id: 8 },
-    { body: "21", correct: false, question_id: 8 },
-    { body: "15", correct: false, question_id: 9 },
-    { body: "5", correct: true, question_id: 9 },
+    { body: "Math", correct: true, question_id: questions[0].id },
+    { body: "Music", correct: false, question_id: questions[0].id },
+    { body: "4", correct: true, question_id: questions[1].id },
+    { body: "5", correct: false, question_id: questions[1].id },
+    { body: "France", correct: true, question_id: questions[2].id },
+    { body: "Germany", correct: false, question_id: questions[2].id },
+    { body: "Paragraph tag", correct: true, question_id: questions[3].id },
+    { body: "Party", correct: false, question_id: questions[3].id },
+    { body: "Hash", correct: false, question_id: questions[4].id },
+    { body: "Array", correct: true, question_id: questions[4].id },
+    { body: "Farrokh Bulsara", correct: true, question_id: questions[5].id },
+    { body: "Freddie Mercury", correct: false, question_id: questions[5].id },
+    { body: "125", correct: true, question_id: questions[6].id },
+    { body: "225", correct: false, question_id: questions[6].id },
+    { body: "11", correct: true, question_id: questions[7].id },
+    { body: "21", correct: false, question_id: questions[7].id },
+    { body: "15", correct: false, question_id: questions[8].id },
+    { body: "5", correct: true, question_id: questions[8].id },
   ]
 )
 
