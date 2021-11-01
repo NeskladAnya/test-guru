b384b2098703dbc3552110ff99d40ff7cd5dfebc
NeskladAnya <belko.hanna.off@gmail.com>
2021-11-01 19:28:30 +0100

    Make required changes

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,8 +1,7 @@
 class Test < ApplicationRecord
 
-  def self.find_test(category)
-    Test
-    .joins('JOIN categories ON tests.category_id = categories.id')
+  def self.find_test_by_category(category)
+    joins('JOIN categories ON tests.category_id = categories.id')
     .where(categories: {title: category})
     .order(title: :desc)
     .pluck('title')
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-  def find_test(level)
-    Result.joins('JOIN tests ON results.test_id = tests.id')
-      .where(user_id: id, tests: {level: level})
-      .pluck('tests.title')
+  def find_test_by_level(level)
+    Test
+      .joins('JOIN results on tests.id = results.test_id')
+      .where(level: level, results: { user_id: id })
   end
 end
