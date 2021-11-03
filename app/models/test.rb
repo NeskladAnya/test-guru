19158a0f2f7781f977db9b11526b9166a71e9dc1
NeskladAnya <belko.hanna.off@gmail.com>
2021-11-03 13:29:21 +0100

    Modify the Result model and results table

--- app/models/test.rb
+++ app/models/test.rb
@@ -3,6 +3,7 @@ class Test < ApplicationRecord
   belongs_to :category
   has_many :results
   has_many :users, through: :results
+  belongs_to :author_id, class_name: 'User', foreign_key: 'author_id'
 
   def self.find_test_by_category(category)
     joins('JOIN categories ON tests.category_id = categories.id')
