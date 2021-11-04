a7c15e4bd0c3b68b4a34d6e8f0ba2ccc9181100a
NeskladAnya <belko.hanna.off@gmail.com>
2021-11-04 13:45:48 +0100

    Fix issues in models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-  has_many :created_tests, class_name: 'Test', foreign_key: 'author_id'
-  has_many :results
-  has_many :tests, through: :results
+  has_many :created_tests, class_name: 'Test', foreign_key: 'author_id', dependent: :destroy
+  has_many :results, dependent: :destroy
+  has_many :tests, through: :results, dependent: :destroy
 
   def find_test_by_level(level)
     tests.where(level: level)
