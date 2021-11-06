8e3499bc9170ed568ccd066ed017258b310fdba0
NeskladAnya <belko.hanna.off@gmail.com>
2021-11-06 12:24:37 +0100

    Add new scope methods

--- app/models/test.rb
+++ app/models/test.rb
@@ -6,6 +6,10 @@ class Test < ApplicationRecord
   has_many :results, dependent: :destroy
   has_many :users, through: :results, dependent: :destroy
 
+  scope :easy, -> { where(level: 0..1) }
+  scope :normal, -> { where(level: 2..4) }
+  scope :hard, -> { where(level: 5..) }
+
   def self.find_test_by_category(category)
     joins(:category)
     .where(categories: {title: category})
