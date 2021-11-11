6eb18421581e286be0c21bed468d1dcf36bb5571
NeskladAnya <belko.hanna.off@gmail.com>
2021-11-11 09:55:44 +0100

    Modify cotroller

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,6 +1,6 @@
 class QuestionsController < ApplicationController
-  before_action :find_test
-  before_action :find_test_questions
+  before_action :find_test, only: %i[index create]
+  before_action :find_test_questions, only: %i[create]
 
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
 
@@ -12,17 +12,23 @@ class QuestionsController < ApplicationController
   end
 
   def create
-    question = @test.questions.create(question_params)
+    @question = @questions.build(question_params)
 
-    render plain: question.inspect
+    if @question.save
+      render inline: '<%= @question.inspect %>'
+    else
+      render :new
+    end
   end
 
   def show
-    render inline: '<%= @questions.find(params[:id]).inspect %>'
+     @question = Question.find(params[:id])
+
+     render plain: @question.inspect
   end
 
   def destroy
-    @questions.find(params[:id]).destroy
+    Question.find(params[:id]).destroy
   end
 
   private
@@ -36,7 +42,7 @@ class QuestionsController < ApplicationController
   end
 
   def question_params
-    params.require[:questions].permit(:body)
+    params.require(:questions).permit(:body)
   end
 
   def rescue_with_question_not_found
