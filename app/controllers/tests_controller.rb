ad21b531307dd66d8b6eba290a1362904cc303be
NeskladAnya <belko.hanna.off@gmail.com>
2021-11-18 21:57:56 +0300

    Add the test edit method and view

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,11 +1,20 @@
 class TestsController < ApplicationController
+  before_action :find_test, only: %i[show edit]
 
   def index
     @tests = Test.all
   end
 
   def show
-    @test = Test.find(params[:id])
     @questions = @test.questions
+  end
+
+  def edit
+  end
+
+  private
+
+  def find_test
+    @test = Test.find(params[:id])
   end
 end
