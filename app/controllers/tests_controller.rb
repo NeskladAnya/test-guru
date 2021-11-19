9eb19b5f9f5d97f7fc0ad7392f923ad0fd0bbe1c
NeskladAnya <belko.hanna.off@gmail.com>
2021-11-19 08:48:42 +0300

    Add update/create/detroy methods

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,5 +1,5 @@
 class TestsController < ApplicationController
-  before_action :find_test, only: %i[show edit]
+  before_action :find_test, only: %i[show edit update destroy]
 
   def index
     @tests = Test.all
@@ -16,9 +16,37 @@ class TestsController < ApplicationController
     @test = Test.new
   end
 
+  def create
+    @test = Test.new(test_params)
+
+    if @test.save
+      redirect_to @test
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @test.update(test_params)
+      redirect_to @test
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @test.destroy
+
+    redirect_to tests_path
+  end
+
   private
 
   def find_test
     @test = Test.find(params[:id])
+  end
+
+  def test_params
+    params.require(:test).permit(:title, :level, :category_id, :author_id)
   end
 end
