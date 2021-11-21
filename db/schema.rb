d074aa228df618a1d2970e27a44ef514ec49af6f
NeskladAnya <belko.hanna.off@gmail.com>
2021-11-21 10:23:41 +0300

    Remove the Reults table and create the Test_Passages instead

--- db/schema.rb
+++ db/schema.rb
@@ -1,16 +1,4 @@
-# This file is auto-generated from the current state of the database. Instead
-# of editing this file, please use the migrations feature of Active Record to
-# incrementally modify your database, and then regenerate this schema definition.
-#
-# This file is the source Rails uses to define your schema when running `bin/rails
-# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
-# be faster and is potentially less error prone than running all of your
-# migrations from scratch. Old migrations may fail to apply correctly if those
-# migrations use external dependencies or application code.
-#
-# It's strongly recommended that you check this file into your version control system.
-
-ActiveRecord::Schema.define(version: 2021_11_21_064946) do
+ActiveRecord::Schema.define(version: 2021_11_21_071808) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
@@ -35,17 +23,16 @@ ActiveRecord::Schema.define(version: 2021_11_21_064946) do
     t.index ["test_id"], name: "index_questions_on_test_id"
   end
 
-  create_table "results", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.integer "test_id", null: false
-    t.integer "grade"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+  create_table "test_passages", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "test_id"
     t.integer "current_question_id"
     t.integer "correct_questions", default: 0
-    t.index ["current_question_id"], name: "index_results_on_current_question_id"
-    t.index ["test_id"], name: "index_results_on_test_id"
-    t.index ["user_id"], name: "index_results_on_user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["current_question_id"], name: "index_test_passages_on_current_question_id"
+    t.index ["test_id"], name: "index_test_passages_on_test_id"
+    t.index ["user_id"], name: "index_test_passages_on_user_id"
   end
 
   create_table "tests", force: :cascade do |t|
@@ -68,9 +55,9 @@ ActiveRecord::Schema.define(version: 2021_11_21_064946) do
 
   add_foreign_key "answers", "questions"
   add_foreign_key "questions", "tests"
-  add_foreign_key "results", "questions", column: "current_question_id"
-  add_foreign_key "results", "tests"
-  add_foreign_key "results", "users"
+  add_foreign_key "test_passages", "questions", column: "current_question_id"
+  add_foreign_key "test_passages", "tests"
+  add_foreign_key "test_passages", "users"
   add_foreign_key "tests", "categories"
   add_foreign_key "tests", "users", column: "author_id"
 end
