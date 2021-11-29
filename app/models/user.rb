f0121758176421e0567cccd8686246edaea927d1
NeskladAnya <belko.hanna.off@gmail.com>
2021-11-29 19:10:05 +0100

    Use bcrypt for auth implementation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
-  include Auth
-
   has_many :created_tests, class_name: 'Test', foreign_key: 'author_id', dependent: :destroy
   has_many :test_passages, dependent: :destroy
   has_many :tests, through: :test_passages, dependent: :destroy
+
+  has_secure_password
 
   def find_test_by_level(level)
     tests.where(level: level)
