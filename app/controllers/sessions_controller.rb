506f0d9e6ceff3a252134b057fc01ff0774ce4af
NeskladAnya <belko.hanna.off@gmail.com>
2021-12-03 16:30:25 +0100

    Add logging out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,4 +13,10 @@ class SessionsController < ApplicationController
       render :new
     end
   end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:notice] = "Logged out"
+    redirect_to login_path 
+  end
 end
