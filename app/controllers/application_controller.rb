620ac8c41ddf73500a35e54262a62a76e547785b
NeskladAnya <belko.hanna.off@gmail.com>
2021-12-11 22:10:32 +0100

    Set resources administration available only for admins

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+
+  def after_sign_in_path_for(resource)
+    flash.notice = "Hello, #{current_user.first_name}!"
+    
+    current_user.is_a?(Admin) ? admin_tests_path : stored_location_for(resource)
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+  end
 end
