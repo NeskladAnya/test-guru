723f56aec9d8116d0ab4de6789c5709609bd5a5a
NeskladAnya <belko.hanna.off@gmail.com>
2021-12-16 16:34:51 +0100

    Set locale

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,12 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  before_action :set_locale
+
+  def default_url_options
+    I18n.default_locale == I18n.locale ? {} : { lang: I18n.locale }
+  end
+
   private
 
   def after_sign_in_path_for(user)
@@ -10,5 +16,9 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+  end
+
+  def set_locale
+    I18n.locale = I18n.locale_available?(params[:lang]) ? params[:lang] : I18n.default_locale
   end
 end
