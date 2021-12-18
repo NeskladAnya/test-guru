d85ea2722f924a66bdf1ee46f382d164197cf764
NeskladAnya <belko.hanna.off@gmail.com>
2021-12-18 09:47:14 +0100

    Style flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,13 @@ module ApplicationHelper
   def project_link
     link_to "Test Guru", root_path, class: "navbar-brand"
   end
+
+  def flash_class(key)
+    case key.to_sym
+    when :notice then "alert alert-info"
+    when :success then "alert alert-success"
+    when :error then "alert alert-error"
+    when :alert then "alert alert-danger"
+    end
+  end
 end
