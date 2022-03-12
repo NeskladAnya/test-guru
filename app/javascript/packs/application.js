0e69c1c864bdb95d60eb94f27baaaae878299b1b
NeskladAnya <belko.hanna.off@gmail.com>
2022-03-12 10:25:41 +0300

    Some changes

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,7 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import 'utilities/sorting'
+import 'unilities/password_match'
 
 Rails.start()
 Turbolinks.start()
