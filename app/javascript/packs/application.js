db9eb0c9202fe4f85ed4830686ab6120470aeee3
Hanna <hanna@Hannas-MacBook-Pro.local>
2022-05-06 20:44:56 +0200

    Add password matching validations

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,7 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import 'utilities/sorting'
-import 'unilities/password_match'
+import 'utilities/password_match'
 
 Rails.start()
 Turbolinks.start()
--- app/javascript/utilities/password_match.js
+++ app/javascript/utilities/password_match.js
@@ -1,3 +1,24 @@
 document.addEventListener('turbolinks:load', function() {
 
+  this.addEventListener('click', checkPasswordFields)
+
+})
+
+function checkPasswordFields() {
+  var password = document.getElementById('user_password').value
+  var password_confirmation = document.getElementById('user_password_confirmation').value
+
+  if (password && password_confirmation) {
+    this.querySelector('.octicon-alert').classList.remove('hide')
+
+    if (password == password_confirmation) {
+      this.querySelector('.octicon-alert').classList.add('text-success')
+      this.querySelector('.octicon-alert').classList.remove('text-danger')
+    } else {
+      this.querySelector('.octicon-alert').classList.add('text-danger')
+      this.querySelector('.octicon-alert').classList.remove('text-success')
+    }
+  } else {
+    this.querySelector('.octicon-alert').classList.add('hide')
+  } 
 }
